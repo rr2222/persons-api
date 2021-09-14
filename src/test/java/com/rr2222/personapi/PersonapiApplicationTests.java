e91d65ef4766b474b5a7699f1ee77c6b71585c96
rr2222 <ruither.carvalho@gmail.com>
2021-09-13 23:11:30 -0300

    first commit

---
+++ src/test/java/com/rr2222/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rr2222.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
