23a143806f662c8cfd6a571582f2d27b6533a85e
rr2222 <ruither.carvalho@gmail.com>
2021-09-15 14:13:50 -0300

    Utilização do DTO para requisiçao

--- src/main/java/com/rr2222/personapi/controller/PersonController.java
+++ src/main/java/com/rr2222/personapi/controller/PersonController.java
@@ -1,12 +1,15 @@
 package com.rr2222.personapi.controller;
 
 import com.rr2222.personapi.dto.MessageResponseDTO;
+import com.rr2222.personapi.dto.request.PersonDTO;
 import com.rr2222.personapi.entity.Person;
 import com.rr2222.personapi.repository.PersonRepository;
 import com.rr2222.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -18,7 +21,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 }
--- src/main/java/com/rr2222/personapi/service/PersonService.java
+++ src/main/java/com/rr2222/personapi/service/PersonService.java
@@ -1,21 +1,37 @@
 package com.rr2222.personapi.service;
 
 import com.rr2222.personapi.dto.MessageResponseDTO;
+import com.rr2222.personapi.dto.request.PersonDTO;
 import com.rr2222.personapi.entity.Person;
+
 import com.rr2222.personapi.repository.PersonRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        ModelMapper modelMapper = new ModelMapper();
+        Person personToSave = modelMapper.map(personDTO, Person.class);
+        personToSave.setBirthDate(convertStringToData(personDTO.getBirthDate()));
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
                 .message("Created person with id" + savedPerson.getId())
                 .build();
+    }
+
+    private LocalDate convertStringToData(String data){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate localDate = LocalDate.parse(data, formatter);
+        return localDate;
     }
 }
