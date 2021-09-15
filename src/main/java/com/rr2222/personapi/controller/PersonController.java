fc0af403031d1db21ed2472bd8cd5dbf863738d5
rr2222 <ruither.carvalho@gmail.com>
2021-09-15 15:42:40 -0300

    Criado metodo GET busca pessoa por ID e criado exception para person not found

--- src/main/java/com/rr2222/personapi/controller/PersonController.java
+++ src/main/java/com/rr2222/personapi/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.rr2222.personapi.controller;
 import com.rr2222.personapi.dto.MessageResponseDTO;
 import com.rr2222.personapi.dto.request.PersonDTO;
 import com.rr2222.personapi.entity.Person;
+import com.rr2222.personapi.exception.PersonNotFoundException;
 import com.rr2222.personapi.repository.PersonRepository;
 import com.rr2222.personapi.service.PersonService;
 import jdk.nashorn.internal.objects.annotations.Getter;
@@ -30,5 +31,10 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAllPersons(){
         return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findPersonById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
--- src/main/java/com/rr2222/personapi/service/PersonService.java
+++ src/main/java/com/rr2222/personapi/service/PersonService.java
@@ -4,8 +4,8 @@ import com.rr2222.personapi.dto.MessageResponseDTO;
 import com.rr2222.personapi.dto.request.PersonDTO;
 import com.rr2222.personapi.entity.Person;
 
+import com.rr2222.personapi.exception.PersonNotFoundException;
 import com.rr2222.personapi.repository.PersonRepository;
-import jdk.nashorn.internal.objects.NativeArray;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,12 +32,6 @@ public class PersonService {
                 .build();
     }
 
-    private LocalDate convertStringToData(String data){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate localDate = LocalDate.parse(data, formatter);
-        return localDate;
-    }
-
     public List<PersonDTO> listAll() {
         List<Person> personList = personRepository.findAll();
         ModelMapper modelMapper = new ModelMapper();
@@ -45,5 +40,20 @@ public class PersonService {
                 .map(user -> modelMapper.map(user, PersonDTO.class))
                 .collect(Collectors.toList());
         return dtos;
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Optional<Person> retorno = personRepository.findById(id);
+        if (!retorno.isPresent()){
+            throw new PersonNotFoundException(id);
+        }
+        ModelMapper modelMapper = new ModelMapper();
+       return modelMapper.map(retorno.get(), PersonDTO.class);
+    }
+
+    private LocalDate convertStringToData(String data){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate localDate = LocalDate.parse(data, formatter);
+        return localDate;
     }
 }
